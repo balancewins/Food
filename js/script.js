e109edc5ac681ebe5a38ae3cf2d5b78f4e3cfd74
balancewins <balancewins@bk.ru>
2022-10-07 06:39:58 +0300

    modifed timer for stop timer

--- js/script.js
+++ js/script.js
@@ -46,11 +46,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2022-10-31';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000*60*60*24)),
-              hours = Math.floor((t / (1000*60*60)) % 24),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        let days, hours, minutes, seconds;
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000*60*60*24));
+            hours = Math.floor((t / (1000*60*60)) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             'total': t,
